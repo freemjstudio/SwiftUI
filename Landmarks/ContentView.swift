a62247284d557ce5b1d1318e60dbbf5be53b735c
freemjstudio <mjwoo0@naver.com>
2021-01-10 17:32:19 +0900

    SwiftUI Tutorial

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+            .ignoresSafeArea(edges: .top)
+            .frame(height: 300)
+            
+        CircleImage()
+            .offset(y: -130)
+            .padding(.bottom, -130)
+        VStack(alignment: .leading) {
+            Text("Turtle Rock").font(.title)
+            
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+            .font(.subheadline)
+            .foregroundColor(.secondary)
+            
+            Divider()
+            
+            Text("About Turtle Rock")
+                .font(.title2)
+            Text("Descriptive text goes here.")
+            
+        }
+            .padding()
+            Spacer()
+        }
+    }
+} //conforms to View protocol & describes View and Content
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    
+        }
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 우민지 on 2021/01/06.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
